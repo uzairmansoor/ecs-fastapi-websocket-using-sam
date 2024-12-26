d5b971b116b094b6a7d754022d7eff9ef9d4c338
uzairmansoor <uzairmansoor123123@gmail.com>
2024-12-27 02:33:56 +0500

    modified routes in app.py

--- fastapi-ecs/app.py
+++ fastapi-ecs/app.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/api")
+async def read_root():
+    return {"message": "Hello from FastAPI"}
+
 @app.get("/health")
 async def root():
     return {"message": "Welcome to the Fast APIs"}
